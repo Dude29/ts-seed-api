2243c86c79551f86accaa8dba6cd3b1ff4abffc8
Dude29 <6997743+Dude29@users.noreply.github.com>
2020-07-21 15:06:01 +0100

    Add clean task to gulpfile

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,6 +4,10 @@ function run(command: string) {
 	return cmd(command)()
 }
 
+export async function clean() {
+	return run('rm -rf build/')
+}
+
 export default async function build() {
 	// MUST return the result of the call to run() in gulp >= v4.0
 	return run('tsc')
