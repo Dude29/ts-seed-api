ccb2d852a3defa30fbe1262373d1f5541aed73cc
Dude29 <6997743+Dude29@users.noreply.github.com>
2020-07-21 15:04:22 +0100

    Slight convenient refactor to avoid the call to run() in gulpfile

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,10 @@
-import run from 'gulp-run-command'
+import cmd from 'gulp-run-command'
+
+function run(command: string) {
+	return cmd(command)()
+}
 
 export default async function build() {
 	// MUST return the result of the call to run() in gulp >= v4.0
-	return run('tsc')()
+	return run('tsc')
 }
