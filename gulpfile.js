a54cd8e6c69223c0b02f8fdb2a096337b9c4d44c
Dude29 <6997743+Dude29@users.noreply.github.com>
2020-07-21 12:34:19 +0100

    Fix gulpfile syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-export default function defaultTask(cb) {
+function defaultTask(cb) {
 	// place code for your default task here
 	cb();
 }
 
+// Gulp needs to use this syntax because it can't parse ES6 constructs yet
+exports.default = defaultTask;
