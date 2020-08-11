220019457aa9ace32c0149cddc89e0174778d804
Dude29 <6997743+Dude29@users.noreply.github.com>
2020-08-11 12:04:14 +0100

    Plugs in previously created route handles to /books endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import books from './books'
 
 export const api = express()
 const port = 3000
@@ -6,6 +7,8 @@ const port = 3000
 api.get('/', (req, res) => {
 	res.end('Hello World!')
 })
+
+api.use('/books', books)
 
 api.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
